b0fa4b5dee6ff50235ac79011adc0909ab9c1fda
Lily Xu <lily.l.xu.18@dartmouth.edu>
2017-05-10 00:57:56 -0400

    going to bed zzz

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
+# Barry Yang and Lily Xu
+# CS 61 Databases
+# Lab 2 part e
+# May 12, 2017
+
+
 import mysql.connector                      # mysql functionality
 import sys                                  # for misc errors
 
 class Database:
     def __init__(self, server, username, password, database):
-    # def __init__(self):
         # initialize db connection
         self.con = mysql.connector.connect(host=server,
                                            user=username,
@@ -35,6 +40,9 @@ class Database:
     def get_cursor(self):
         return self.cursor
 
+    def get_logged_in(self):
+        return self.logged_in
+
     def get_user_id(self):
         return self.user_id
 
@@ -44,11 +52,16 @@ class Database:
     def change_user_id(self, id):
         self.user_id = int(id)
 
-    def change_user_type(self, type):
-        self.user_type = type
+    def change_user_type(self, user_type):
+        self.user_type = user_type
 
     def log_on(self):
         self.logged_in = True
 
     def log_off(self):
         self.logged_in = False
+
+    def cleanup(self):
+        self.con.close()
+        self.cursor.close()
+
