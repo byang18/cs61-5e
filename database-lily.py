f4671319b1b18f5b2d0e0c5c211748d51009a15b
Lily Xu <lily.l.xu.18@dartmouth.edu>
2017-05-09 23:32:14 -0400

    added framework for all the commands for author, reviewer, and editor

--- database-lily.py
+++ database-lily.py
@@ -13,6 +13,7 @@ DATABASE  = "byang_db"                      # db to user
 query     = "SELECT * FROM feedback;"       # query statement
 logged_in = False
 user_id   = -1                              # current user id
+user_type = None                            # author, editor, or reviewer
 
 if __name__ == "__main__":
     try:
@@ -81,7 +82,21 @@ def parse_input(string):
 def login(id):
     # execute login
     # find corresponding id
+
+    # TODO: check if user_id is valid?
     user_id = int(id)
+    logged_in = True
+    user_type = "author" # or "editor" or "reviewer"
+
+    if user_type == "author":
+        print "fname, lname, address\n"
+        status_author()
+    elif user_type == "editor":
+        print "fname, lname"
+        status_editor()
+    elif user_type == "reviewer":
+        print "Welcome back, fname, lname!"
+        status_reviewer() # limited to manuscripts assigned to that reviewer
 
 
 def register(tokens):
@@ -93,15 +108,131 @@ def register(tokens):
         register_author(tokens[2], tokens[3])
 
 
-def register_editor(fname, lname):
-    query = "INSERT INTO editor VALUES (NULL, fname, lname);"
-
 
 def register_author(fname, lname, email, address):
     query = "INSERT INTO author VALUES (NULL, fname, lname, email, address);"
 
 
+def register_editor(fname, lname):
+    query = "INSERT INTO editor VALUES (NULL, fname, lname);"
+
+
 def register_reviewer(fname, lname, ricode1, ricode2, ricode3):
     query = "INSERT INTO reviewer VALUES (NULL, fname, lname, ricode1, ricode2, ricode3);"
+
+
+def process_author(tokens):
+    command = tokens[0]
+
+    if command == "status":
+        status_author()
+    elif command == "submit":
+        title = tokens[1]
+        affiliation = tokens[2]
+        RICode = tokens[3]
+        author2 = tokens[4]
+        author3 = tokens[5]
+        author4 = tokens[6]
+        filename = tokens[7]
+        # submit manuscript into system
+    elif command == "retract":
+        manuscript_num = tokens[1]
+        # prompt: are you sure?
+        # if yes, immediately remove manuscript regardless of sattus
+
+
+
+def process_editor(tokens):
+    command = tokens[0]
+
+    if command == "status":
+        status_editor()
+    elif command == "assign":
+        manuscript_num = tokens[1]
+        reviewer_id = tokens[2]
+        # TODO: assign manuscript to a reviewer 
+    elif command == "reject":
+        manuscript_num = tokens[1]
+        # TODO: set manuscript status to rejected and add timestamp
+    elif command == "accept":
+        manuscript_num = tokens[1]
+        # TODO: set manuscript status to accepted and add timestamp
+    elif command == "typeset":
+        manuscript_num = tokens[1]
+        pp = tokens[2]
+        # TODO: set manuscript status to typeset and store num pages
+    elif command == "schedule":
+        manuscript_num = tokens[1]
+        issue = tokens[2]
+        # TODO: set manuscript status to scheduled and add issue page
+        # TODO: if addition of manuscript would cause issue to exceed 100 pages, schedule request should fail
+    elif command == "publish":
+        issue = tokens[2]
+        # TODO: for all manuscripts in issue, set status as published and record timestamp
+
+
+def process_reviewer(tokens):
+    command = tokens[0]
+
+    if command == "status":
+        status_reviewer()
+    elif command == "resign":
+        # prompt to enter unique ID
+        print "Thank you for our service!"
+        # TODO: remove user from system (invoke trigger)
+    elif command == "reject":
+        manuscript_num = tokens[1]
+        appropriateness = tokens[2]
+        clarity = tokens[3]
+        methodology = tokens[4]
+        contribution = tokens[5]
+        # TODO: add info; mark rejected; timestamp
+    elif command == "accept":
+        manuscript_num = tokens[1]
+        appropriateness = tokens[2]
+        clarity = tokens[3]
+        methodology = tokens[4]
+        contribution = tokens[5]
+        # TODO: add info; mark accepted; timestamp
+    # TODO: if attempt to act on manuscript not assigned to reviewer, display appropriate fail message
+
+
+
+
+def status_author():
+    x = 5
+
+    print("{} submitted".format(x))
+    print("{} under review".format(x))
+    print("{} rejected".format(x))
+    print("{} accepted".format(x))
+    print("{} in typesetting".format(x))
+    print("{} scheduled for publication".format(x))
+    print("{} published".format(x))
+
+
+def status_editor():
+    x = 5
+
+    print("{} submitted".format(x))
+    print("{} under review".format(x))
+    print("{} rejected".format(x))
+    print("{} accepted".format(x))
+    print("{} in typesetting".format(x))
+    print("{} scheduled for publication".format(x))
+    print("{} published".format(x))
+
+
+def status_reviewer():
+    x = 5
+
+    print("{} submitted".format(x))
+    print("{} under review".format(x))
+    print("{} rejected".format(x))
+    print("{} accepted".format(x))
+    print("{} typesetting".format(x))
+    print("{} scheduled for publication".format(x))
+    print("{} published".format(x))
+
 
 
