6e57651262b25b809b053ca1262deba5d5cb9098
Barry <barry.a.yang.18@dartmouth.edu>
2017-05-09 23:54:46 -0400

    barry

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
+# pylint: disable=C0111, C0326, W0702, C0103
+
 from __future__ import print_function        # make print a function
-import mysql.connector                       # mysql functionality
 import sys                                   # for misc errors
+import mysql.connector                       # mysql functionality
+
 
 SERVER   = "sunapee.cs.dartmouth.edu"        # db server to connect to
 USERNAME = "byang"                            # user to connect as
@@ -8,37 +11,57 @@ PASSWORD = "7webster"                            # user's password
 DATABASE = "byang_db"                              # db to user
 QUERY    = "SELECT * FROM feedback;"       # query statement
 
+def main():
+    try:
+        # initialize db connection
+        con = mysql.connector.connect(host=SERVER,user=USERNAME,password=PASSWORD,
+                                      database=DATABASE)
+        print("Connection established.")
+
+        # initialize a cursor
+        cursor = con.cursor()
+
+        # query db
+        cursor.execute(QUERY)
+
+        # print("Query executed: '{0}'\n\nResults:".format(QUERY))
+
+        # print table header
+        # print("".join(["{:<20}".format(col) for col in cursor.column_names]))
+        # print("------------------------------------------------------------------")
+
+        # iterate through results
+        # for row in cursor
+        # print("".join(["{:<20}".format(col) for col in row]))
+
+    except mysql.connector.Error as e:        # catch SQL errors
+        print("SQL Error: {0}".format(e.msg))
+    except:                                   # anything else
+        print("Unexpected error: {0}".format(sys.exc_info()[0]))
+
+    raw = raw_input('--> ')
+
+    while raw != "quit":
+        print(raw)
+        input_list = raw.split()
+        print(input_list)
+
+        if input_list[0] == "login":
+            print("login code here")
+
+        elif input_list[0] == "author":
+            print("login code here")
+
+        elif input_list[0] == "author":
+            print("login code here")
+
+        raw = raw_input('--> ')
+
+    # cleanup
+    con.close()
+    cursor.close()
+
+    print("\nConnection terminated.", end='')
+
 if __name__ == "__main__":
-   try:
-      # initialize db connection
-      con = mysql.connector.connect(host=SERVER,user=USERNAME,password=PASSWORD,
-                                    database=DATABASE)
-
-      print("Connection established.")
-
-      # initialize a cursor
-      cursor = con.cursor()
-
-      # query db
-      cursor.execute(QUERY)
-
-      print("Query executed: '{0}'\n\nResults:".format(QUERY))
-
-      # print table header
-      print("".join(["{:<20}".format(col) for col in cursor.column_names]))
-      print("------------------------------------------------------------------")
-
-      # iterate through results
-      for row in cursor:
-         print("".join(["{:<20}".format(col) for col in row]))
-
-   except mysql.connector.Error as e:        # catch SQL errors
-      print("SQL Error: {0}".format(e.msg))
-   except:                                   # anything else
-      print("Unexpected error: {0}".format(sys.exc_info()[0]))
-
-   # cleanup
-   con.close()
-   cursor.close()
-
-   print("\nConnection terminated.", end='')
+    main()
--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,7 @@
+# pylint: disable=C0111, C0103
+
 name = raw_input("Enter your name: ")
-print name
+
+while name != "quit":
+    print name
+    name = raw_input("Enter your name: ")
